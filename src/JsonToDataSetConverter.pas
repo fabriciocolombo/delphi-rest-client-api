f76154b766ca2122017deae2c572db628e480539
Fabricio Colombo <fabricio.colombo.mva@gmail.com>
2012-09-16 14:30:45 -0300

    Desserialization from Json to DataSet with NestedDataSets.

--- src/JsonToDataSetConverter.pas
+++ src/JsonToDataSetConverter.pas
@@ -8,8 +8,10 @@ type
   TJsonToDataSetConverter = class
   private
     class procedure AppendRecord(ADataSet: TDataSet; AObject: ISuperObject);
+    class procedure SetFieldValue(AField: TField; AValue: ISuperObject);
   public
-    class procedure UnMarshalToDataSet(ADataSet: TDataSet; AJson: string);
+    class procedure UnMarshalToDataSet(ADataSet: TDataSet; AJson: string);overload;
+    class procedure UnMarshalToDataSet(ADataSet: TDataSet; AObject: ISuperObject);overload;
   end;
 
 implementation
@@ -18,54 +20,91 @@ implementation
 
 class procedure TJsonToDataSetConverter.AppendRecord(ADataSet: TDataSet;AObject: ISuperObject);
 var
-  i: Integer;
   vField: TField;
-  vDate: TDateTime;
+  vIterator: TSuperObjectIter;
 begin
   ADataSet.Append;
-  for i := 0 to ADataSet.FieldCount-1 do
-  begin
-    vField := ADataSet.Fields[i];
 
-    case vField.DataType of
-      ftSmallint, ftInteger, ftWord, ftLargeint: vField.AsInteger := AObject.I[vField.FieldName];
-      ftFloat, ftCurrency, ftBCD, ftFMTBcd: vField.AsFloat := AObject.D[vField.FieldName];
-      ftBoolean: vField.AsBoolean := AObject.B[vField.FieldName];
-      ftDate, ftTime, ftDateTime, ftTimeStamp:  begin
-                                                  if SuperObject.ISO8601DateToDelphiDateTime(AObject.S[vField.FieldName], vDate) then
-                                                  begin
-                                                    vField.AsDateTime := vDate;
-                                                  end;
-                                                end;
-      ftDataSet: ;
-    else
-      vField.AsString := AObject.S[vField.FieldName];
+  if SuperObject.ObjectFindFirst(AObject, vIterator) then
+  begin
+    try
+      repeat
+        vField := ADataSet.FindField(vIterator.key);
+
+        if Assigned(vField) then
+        begin
+          SetFieldValue(vField, vIterator.val);
+        end;
+      until not SuperObject.ObjectFindNext(vIterator);
+    finally
+      SuperObject.ObjectFindClose(vIterator);
     end;
   end;
+
   ADataSet.Post;
+end;
+
+class procedure TJsonToDataSetConverter.SetFieldValue(AField: TField;AValue: ISuperObject);
+var
+  vFieldName: string;
+  vDate: TDateTime;
+  vNestedDataSet: TDataSet;
+begin
+  vFieldName := AField.FieldName;
+  case AField.DataType of
+    ftSmallint, ftInteger, ftWord, ftLargeint: AField.AsInteger := AValue.AsInteger;
+    ftFloat, ftCurrency, ftBCD, ftFMTBcd: AField.AsFloat := AValue.AsDouble;
+    ftBoolean: AField.AsBoolean := AValue.AsBoolean;
+    ftDate, ftTime, ftDateTime, ftTimeStamp:  begin
+                                                if SuperObject.ISO8601DateToDelphiDateTime(AValue.AsString, vDate) then
+                                                begin
+                                                  AField.AsDateTime := vDate;
+                                                end;
+                                              end;
+    ftDataSet:  begin
+                  vNestedDataSet := TDataSetField(AField).NestedDataSet;
+
+                  UnMarshalToDataSet(vNestedDataSet, AValue);
+                end;
+  else
+    AField.AsString := AValue.AsString;
+  end;
+end;
+
+class procedure TJsonToDataSetConverter.UnMarshalToDataSet(ADataSet: TDataSet;AObject: ISuperObject);
+var
+  i: Integer;
+  vArray: TSuperArray;
+begin
+  ADataSet.DisableControls;
+  try
+    if AObject.IsType(stArray) then
+    begin
+      vArray := AObject.AsArray;
+
+      for I := 0 to vArray.Length-1 do
+      begin
+        AppendRecord(ADataSet, vArray.O[i]);
+      end;
+    end
+    else
+    begin
+      AppendRecord(ADataSet, AObject);
+    end;
+  finally
+    ADataSet.EnableControls;
+  end;
+
+  ADataSet.First;
 end;
 
 class procedure TJsonToDataSetConverter.UnMarshalToDataSet(ADataSet: TDataSet; AJson: string);
 var
-  i: Integer;
-  obj: ISuperObject;
-  vArray: TSuperArray;
+  AObject: ISuperObject;
 begin
-  obj := SuperObject.SO(AJson);
+  AObject := SuperObject.SO(AJson);
 
-  if obj.IsType(stArray) then
-  begin
-    vArray := obj.AsArray;
-
-    for I := 0 to vArray.Length-1 do
-    begin
-      AppendRecord(ADataSet, vArray.O[i]);
-    end;
-  end
-  else
-  begin
-    AppendRecord(ADataSet, obj);
-  end;
+  UnMarshalToDataSet(ADataSet, AObject);
 end;
 
 end.
--- unittest/TestDataSetHandler.pas
+++ unittest/TestDataSetHandler.pas
@@ -11,13 +11,15 @@ uses BaseTestRest, Classes, IdHttp, RestClient, RestUtils, SuperObject,
 type
   TTestDataSetHandler = class(TBaseTestRest)
   private
-    FDataSet: TDataSet;
+    FDataSet: TClientDataSet;
   protected
     procedure SetUp; override;
     procedure TearDown; override;
   published
     procedure JsonToDataSet;
+    procedure JsonToDataSetTwoRecords;
     procedure JsonSomeTypesToDataSet;
+    procedure JsonToDataSetWithNestedDataSet;
   end;
 
 implementation
@@ -38,6 +40,66 @@ begin
 
   CheckEquals(123, FDataSet.FieldByName('id').AsInteger);
   CheckEqualsString('Fabricio Colombo', FDataSet.FieldByName('name').AsString);
+end;
+
+procedure TTestDataSetHandler.JsonToDataSetTwoRecords;
+var
+  vJson: string;
+begin
+  vJson := '[{"id":1,"name":"Fabricio Colombo"},' +
+           ' {"id":2,"name":"Jacob"}]';
+
+  CheckEquals(0, FDataSet.RecordCount);
+  TJsonToDataSetConverter.UnMarshalToDataSet(FDataSet, vJson);
+  CheckEquals(2, FDataSet.RecordCount);
+
+  CheckEquals(1, FDataSet.FieldByName('id').AsInteger);
+  CheckEqualsString('Fabricio Colombo', FDataSet.FieldByName('name').AsString);
+
+  FDataSet.Next;
+
+  CheckEquals(2, FDataSet.FieldByName('id').AsInteger);
+  CheckEqualsString('Jacob', FDataSet.FieldByName('name').AsString);
+end;
+
+procedure TTestDataSetHandler.JsonToDataSetWithNestedDataSet;
+var
+  vJson: string;
+  vAddresses: TDataSet;
+begin
+  vJson := '{' +
+           '   "id": 123,' +
+           '   "name": "Fabricio Colombo",' +
+           '   "addresses": [' +
+           '       {' +
+           '           "id": 1,' +
+           '           "address": "street one"' +
+           '       },' +
+           '       {' +
+           '           "id": 2,' +
+           '           "address": "street two"' +
+           '       }' +
+           '   ]' +
+           '}';
+
+  CheckEquals(0, FDataSet.RecordCount);
+  TJsonToDataSetConverter.UnMarshalToDataSet(FDataSet, vJson);
+  CheckEquals(1, FDataSet.RecordCount);
+
+  CheckEquals(123, FDataSet.FieldByName('id').AsInteger);
+  CheckEqualsString('Fabricio Colombo', FDataSet.FieldByName('name').AsString);
+
+  vAddresses := TDataSetField(FDataSet.FieldByName('addresses')).NestedDataSet;
+
+  CheckFalse(vAddresses.IsEmpty, 'Addresses is empty');
+
+  CheckEquals(1, vAddresses.FieldByName('id').AsInteger);
+  CheckEqualsString('street one', vAddresses.FieldByName('address').AsString);
+
+  vAddresses.Next;
+
+  CheckEquals(2, vAddresses.FieldByName('id').AsInteger);
+  CheckEqualsString('street two', vAddresses.FieldByName('address').AsString);
 end;
 
 procedure TTestDataSetHandler.JsonSomeTypesToDataSet;
@@ -63,15 +125,40 @@ begin
 end;
 
 procedure TTestDataSetHandler.SetUp;
+
+  function CreateField(DataSet: TDataSet; FieldType: TFieldType; const FieldName: string = '';ASize: Integer=0): TField;
+  begin
+    Result:= DefaultFieldClasses[FieldType].Create(DataSet);
+    Result.FieldName:= FieldName;
+    if Result.FieldName = '' then
+      Result.FieldName:= 'Field' + IntToStr(DataSet.FieldCount +1);
+    Result.FieldKind := fkData;
+    Result.DataSet:= DataSet;
+    Result.Name:= DataSet.Name + Result.FieldName;
+    Result.Size := ASize;
+
+    if (FieldType = ftString) and (ASize <= 0) then
+      raise Exception.CreateFmt('Size não definido para o campo "%s".',[FieldName]);
+  end;
+
+var
+  vAddress: TDataSetField;
 begin
   inherited;
   FDataSet := TClientDataSet.Create(nil);
-  FDataSet.FieldDefs.Add('id', ftInteger);
-  FDataSet.FieldDefs.Add('name', ftString, 60);
-  FDataSet.FieldDefs.Add('value', ftFloat);
-  FDataSet.FieldDefs.Add('active', ftBoolean);
-  FDataSet.FieldDefs.Add('createDate', ftDateTime);
-  TClientDataSet(FDataSet).CreateDataSet;
+
+  CreateField(FDataSet, ftInteger, 'id');
+  CreateField(FDataSet, ftString, 'name', 60);
+  CreateField(FDataSet, ftFloat, 'value');
+  CreateField(FDataSet, ftBoolean, 'active');
+  CreateField(FDataSet, ftDateTime, 'createDate');
+
+  vAddress := TDataSetField(CreateField(FDataSet, ftDataSet, 'addresses'));
+
+  CreateField(vAddress.NestedDataSet, ftInteger, 'id');
+  CreateField(vAddress.NestedDataSet, ftString, 'address', 60);
+
+  FDataSet.CreateDataSet;
 end;
 
 procedure TTestDataSetHandler.TearDown;
