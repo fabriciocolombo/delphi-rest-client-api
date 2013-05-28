d6b21e566440edbdae3f364f9d4bce31134f02b5
Fabricio Colombo <fabricio.colombo.mva@gmail.com>
2013-05-27 23:23:31 -0300

    Compiler directive for XE4

--- src/HttpConnectionIndy.pas
+++ src/HttpConnectionIndy.pas
@@ -116,7 +116,7 @@ begin
 
     if FEnabledCompression then
     begin
-	  //NOTE: This does not work in Delphi XE2. Access violation occurs. 
+      {$Message Warn 'TIdCompressorZLib does not work properly in Delphi XE2. Access violation occurs.'}
       FIdHttp.Compressor := TIdCompressorZLib.Create(FIdHttp);
     end
     else
