051b0637ef63492a5722657d6899b2df857a8221
reckface <reckface@users.noreply.github.com>
2015-04-07 10:54:57 +0100

    Introduced optional DisplayName parameter
    The Field Definition can now have a display name instead of the json-determined fieldName

--- src/DataSetUtils.pas
+++ src/DataSetUtils.pas
@@ -7,7 +7,7 @@ uses DB, SysUtils;
 type
   TDataSetUtils = class
   public
-    class function CreateField(DataSet: TDataSet; FieldType: TFieldType; const FieldName: string = '';ASize: Integer=0): TField;
+    class function CreateField(DataSet: TDataSet; FieldType: TFieldType; const FieldName: string = '';ASize: Integer=0; const displayName: string=''): TField;
     class function CreateDataSetField(DataSet: TDataSet; const FieldName: string): TDataSetField;
   end;
 
@@ -21,19 +21,21 @@ begin
 end;
 
 class function TDataSetUtils.CreateField(DataSet: TDataSet;
-  FieldType: TFieldType; const FieldName: string; ASize: Integer): TField;
+  FieldType: TFieldType; const FieldName: string; ASize: Integer; const displayName: string): TField;
 begin
     Result:= DefaultFieldClasses[FieldType].Create(DataSet);
     Result.FieldName:= FieldName;
     if Result.FieldName = '' then
       Result.FieldName:= 'Field' + IntToStr(DataSet.FieldCount +1);
+    if(displayName <> '') then
+      result.DisplayLabel := displayName;
     Result.FieldKind := fkData;
     Result.DataSet:= DataSet;
     Result.Name:= DataSet.Name + Result.FieldName;
     Result.Size := ASize;
 
     if (FieldType = ftString) and (ASize <= 0) then
-      raise Exception.CreateFmt('Size não definido para o campo "%s".',[FieldName]);
+      raise Exception.CreateFmt('Size nÃ£o definido para o campo "%s".',[FieldName]);
 end;
 
 end.
