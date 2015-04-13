09d26f30c3c5597a6ba920f11ae44c09b2588600
Koltsov <vityarikk@gmail.com>
2015-04-13 10:04:36 +0500

    test for type check

---
+++ Test/DelphiXE3_RestApiTests.dpr
@@ -0,0 +1,26 @@
+program DelphiXE3_RestApiTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  TestRestJsonUtils in 'TestRestJsonUtils.pas';
+
+{R *.RES}
+
+begin
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
